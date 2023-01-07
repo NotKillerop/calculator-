8002ee21f45b419a30167418223fb328172fd624
NotKillerop <109949802+NotKillerop@users.noreply.github.com>
2023-01-07 15:23:46 +0530

    Add files via upload

---
+++ calculator/calculator.java
@@ -0,0 +1,16 @@
+public class calculator
+{
+    public static void main(int a , int b)
+    {
+        int add = a + b ;
+        int subtract = a - b ;
+        int multiply = a * b;
+        int divide = a / b;
+        int modulus = a % b;
+        System.out.println("The sum is =" + add);
+        System.out.println("The subract  is =" + subtract);
+        System.out.println("The product is =" + multiply);
+        System.out.println("The division  is =" + divide);
+        System.out.println("The remainder is =" + modulus);
+    }
+}
